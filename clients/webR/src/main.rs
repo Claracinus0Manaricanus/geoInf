deefef3d6da14fc136ec6c735201746d4b1b21da
Claracinus0Manaricanus <akkayasarp4@gmail.com>
2023-10-07 21:27:47 +0300

    i forgot

--- clients/webR/src/main.rs
+++ clients/webR/src/main.rs
@@ -1,9 +1,17 @@
 use yew::prelude::*;
+use yew::events::*;
 
 #[function_component(App)]
 fn app()->Html{
+    let cuteVar:&str="yep";
     html!{
-        <h1>{"Sample Text"}</h1>
+        <>
+            <div style="inline-size:50%;block-size:100%;background-color:rgba(154,143,200,0.5);padding:10px;">
+                <h1>{"Sample Text"}</h1>
+                <p style="color:red;padding:4px;">{cuteVar}</p>
+                <button>{"place of text for thy button"}</button>
+            </div>
+        </>
     }
 }
 
