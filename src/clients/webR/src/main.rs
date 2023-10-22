0a07232c2de069a517163ffb1640935e689fd794
Claracinus0Manaricanus <akkayasarp4@gmail.com>
2023-10-22 21:16:36 +0300

    CMake fix

--- src/clients/webR/src/main.rs
+++ src/clients/webR/src/main.rs
@@ -7,7 +7,7 @@ fn app()->Html{
     html!{
         <>
             <div style="inline-size:50%;block-size:100%;background-color:rgba(154,143,200,0.5);padding:10px;">
-                <h1>{"Sample Text"}</h1>
+                <h1>{"DEV V"}</h1>
                 <p style="color:red;padding:4px;">{cuteVar}</p>
                 <button>{"place of text for thy button"}</button>
             </div>
